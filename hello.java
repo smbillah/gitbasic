12c8827b502ff0d63f5f5a53875c53392f1cb09e
Syed M. Billah <sbillah@psu.edu>
2020-03-31 13:29:28 -0400

    Add some computation

--- hello.java
+++ hello.java
@@ -2,6 +2,9 @@ public class hello{
 	
 	public void doSomethingIntering(){
 		//doing interesting stuffs here
+		int a = 10;
+		int b = 20;
+		a = a+ b;
 	}
 	
 	public static void main(String[] args){
