e911a9c1461a29a436c18cbbbecffd77733765a8
Billah <skb5969@psu.edu>
2020-03-31 12:44:39 -0400

    added an interesting function

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
 	
+	public void doSomethingIntering(){
+		//doing interesting stuffs here
+	}
+	
 	public static void main(String[] args){
 		System.out.println("Hello Git");
 	}
